efd373fdf4b429aeb168031f7dc33c280dcf7d4a
Muresan Ionut <muresan.ionut95@gmail.com>
2015-08-04 14:12:34 +0300

    upload file

--- app/models/attachment.rb
+++ app/models/attachment.rb
@@ -2,6 +2,8 @@ class Attachment < ActiveRecord::Base
 
   belongs_to :job
 
+  mount_uploader :file
+
   validates :job_id, presence: true
   validates :status, presence: true
   validates :file, presence: true
