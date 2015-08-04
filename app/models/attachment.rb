f5d70dc6c5483e5d7a7114aff12a150cf5d25f7b
Muresan Ionut <muresan.ionut95@gmail.com>
2015-08-04 13:57:37 +0300

    job has many attachment validate presence

--- app/models/attachment.rb
+++ app/models/attachment.rb
@@ -1,2 +1,9 @@
 class Attachment < ActiveRecord::Base
+
+  belongs_to :job
+
+  validates :job_id, presence: true
+  validates :status, presence: true
+  validates :file, presence: true
+
 end
--- app/models/job.rb
+++ app/models/job.rb
@@ -1,2 +1,8 @@
 class Job < ActiveRecord::Base
+
+  has_many :attachments
+
+  validates :title, presence: true
+  validates :description, presence: true
+
 end
