5c8796461edbcf4b8ffba210fec46fe9d3951d3a
Muresan Ionut <muresan.ionut95@gmail.com>
2015-08-06 12:20:18 +0300

    attachment expire in 30 days

--- app/controllers/attachments_controller.rb
+++ app/controllers/attachments_controller.rb
@@ -1,10 +1,14 @@
 class AttachmentsController < ApplicationController
-  api :GET, '/attachments', 'Return all jobs'
+  api :GET, '/attachments', 'Return all attachments'
   def index
-    @attachments = Attachment.where(job_id: params[:job_id]).all
+    @attachments = Attachment.where(
+      'created_at >= :days and job_id == :job_id',
+      :days  => Time.now - 30.days,
+      :job_id => params[:job_id]
+    ).all
   end
 
-  api :GET, '/attachments/:id', 'Return all jobs'
+  api :GET, '/attachments/:id', 'Return attachment by id'
   def show
     begin
       @attachment = Attachment.find(params[:id])
