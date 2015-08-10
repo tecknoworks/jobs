b40d7efd762f18b2610ae743a3b258b833700fc3
Muresan Ionut <muresan.ionut95@gmail.com>
2015-08-10 14:13:44 +0300

    use constants in JobController (#16)

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,13 +1,11 @@
 class JobsController < ApplicationController
   api :GET, '/jobs', 'Return all jobs'
   def index
-    # CODE: magic value
-    @jobs = Job.where(status: 1)
+    @jobs = Job.where(status: Job::PUBLISHED)
   end
 
   api :GET, '/jobs/:id', 'Return one job by id'
   def show
-    # CODE: magic value
-    @job = Job.where(id: params[:id], status: 1).first
+    @job = Job.where(id: params[:id], status: Job::PUBLISHED).first
   end
 end
