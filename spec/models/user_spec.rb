15d7c86f174b54a2e48abc48c88c214badb81aff
Muresan Ionut <muresan.ionut95@gmail.com>
2015-08-10 14:46:06 +0300

    use let

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,24 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  # CODE: use let
+
+  let(:user) {create :user}
 
   it { expect(subject).to validate_presence_of :email }
   it { expect(subject).to validate_presence_of :password }
 
   it 'create user' do
     expect do
-      User.create(email: 'test@example.ro', password: 'password')
+      user
     end.to change { User.count }.by 1
   end
 
   it 'create user when account exist' do
     expect do
-      User.create(email: 'test@example.ro', password: 'password')
+      user
     end.to change { User.count }.by 1
 
     expect do
-      User.create(email: 'test@example.ro', password: 'password')
+      user
     end.to change { User.count }.by 0
   end
 end
