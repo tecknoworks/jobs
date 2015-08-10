e9db06f2ed9c42a070fee4f6aad8f883f5a0a323
Muresan Ionut <muresan.ionut95@gmail.com>
2015-08-10 18:01:04 +0300

    create tests for candidate

--- app/models/candidate.rb
+++ app/models/candidate.rb
@@ -1,10 +1,10 @@
 class Candidate < ActiveRecord::Base
   belongs_to :job
   has_many :attachments
-  
+
   validates :full_name, presence: true
   validates :phone_number, presence: true
   validates :email, presence: true
-  validates :job_id, presence: true
+  validates :job, presence: true
 
 end
--- spec/models/candidate_spec.rb
+++ spec/models/candidate_spec.rb
@@ -1,5 +1,28 @@
 require 'rails_helper'
 
 RSpec.describe Candidate, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:candidate) { create :candidate }
+
+  it { expect(subject).to have_many :attachments }
+  it { expect(subject).to belong_to :job }
+
+  it { expect(subject).to validate_presence_of :full_name }
+  it { expect(subject).to validate_presence_of :phone_number }
+  it { expect(subject).to validate_presence_of :email }
+  it { expect(subject).to validate_presence_of :job }
+
+  it 'work' do
+    expect do
+      candidate
+    end.to change {Candidate.count}.by 1
+  end
+
+  it 'create candidate' do
+    job = Job.create!(description: 'test')
+    candidate = create :candidate, job_id: job.id
+    expect(candidate.job_id).to eq(job.id)
+    expect(candidate.phone_number).to eq('0755555555')
+    expect(candidate.email).to eq('email@example.com')
+    expect(candidate.full_name).to eq('Full Name')
+  end
 end
