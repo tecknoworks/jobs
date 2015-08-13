463ae9ceff7aab0b78193adc83f045d8a68ae1f4
Muresan Ionut <muresan.ionut95@gmail.com>
2015-08-13 14:08:26 +0300

    put image on jobs_page

--- public/js/controllers/JobsController.js
+++ public/js/controllers/JobsController.js
@@ -10,4 +10,26 @@ app.controller('JobsController', function ($scope, $http, $location) {
   $scope.goTo = function (job) {
     $location.url('/jobs/' + job.id);
   }
+  
+  images_name = ['career-business-analyst',
+  'career-business-domain-expert',
+  'career-data-modeller',
+  'career-developer',
+  'career-graphic-designer',
+  'career-infrastructure-architect',
+  'career-performance-tester',
+  'career-project-manager',
+  'career-security-tester',
+  'career-support-analyst',
+  'career-tech-lead',
+  'career-tester',
+  'career-ux-designer']
+
+  var randomNumber = Math.floor(Math.random() * 12);
+  var imgName = images_name[randomNumber] + '.jpg';
+  $scope.number = imgName
+  document.getElementById("imageid").src = '/img/' + imgName ;
+
+
+
 });
