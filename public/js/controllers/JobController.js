05d4f6b150a4ad7cbdd0477c82ea7df909a9c2fb
Muresan Ionut <muresan.ionut95@gmail.com>
2015-08-26 12:59:14 +0300

    export to pdf

--- public/js/controllers/JobController.js
+++ public/js/controllers/JobController.js
@@ -10,6 +10,19 @@ app.controller('JobController', function ($scope, $http, $routeParams) {
 		$scope.html_content = markdown.toHTML( md_content );
 		$scope.posted = moment($scope.job.posted_at, "YYYYMMDD").fromNow();
 		$("markout").innerHTML = $scope.html_content
-	});
 
+	$scope.export_to_pdf = function(){
+		var doc = new jsPDF();
+
+		doc.fromHTML(
+		  $scope.html_content,
+		  15,
+		  15,
+		  {
+			    'width': 180
+		  }
+		);
+		doc.output("dataurlnewwindow");
+		};
+	});
 });
