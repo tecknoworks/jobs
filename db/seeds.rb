758880204f3a14ca8219725c32ad1ed3c4289d23
Muresan Ionut <muresan.ionut95@gmail.com>
2015-08-31 14:23:55 +0300

    create users, interviews, attachments and candidates from seed (#35)

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,3 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
-
 Dir['db/jobs/*.md'].each do |path|
   file = File.read(path).strip
   status = Job::PUBLISHED
@@ -13,3 +5,30 @@ Dir['db/jobs/*.md'].each do |path|
   status = Job::DRAFT if path.starts_with?('db/jobs/01_')
   Job.create!(description: file, status: status)
 end
+
+user1 = User.create!(email: 'ionut@example.com', password: 'password')
+user2 = User.create!(email: 'catalin@example.com', password: 'password')
+user3 = User.create!(email: 'test@example.com', password: 'password')
+user4 = User.create!(email: 'foo@example.com', password: 'password')
+
+candidate1 = Candidate.create!(job_id: 2, full_name: 'Ghita', phone_number: '0745454545', email: 'ghita@example.com')
+candidate2 = Candidate.create!(job_id: 2, full_name: 'Alexandru', phone_number: '0722222222', email: 'alexandru@example.com')
+candidate3 = Candidate.create!(job_id: 3, full_name: 'Ioana', phone_number: '0733333333', email: 'ioana@example.com')
+candidate4 = Candidate.create!(job_id: 2, full_name: 'Andreea', phone_number: '0744444444', email: 'andreea@example.com')
+
+Attachment.create!(user_id: user1.id, candidate_id: candidate1.id, file: Rack::Test::UploadedFile.new('spec/erd.pdf'))
+Attachment.create!(user_id: user1.id, candidate_id: candidate2.id, file: Rack::Test::UploadedFile.new('spec/erd.pdf'))
+Attachment.create!(user_id: user1.id, candidate_id: candidate3.id, file: Rack::Test::UploadedFile.new('spec/erd.pdf'))
+Attachment.create!(user_id: user2.id, candidate_id: candidate4.id, file: Rack::Test::UploadedFile.new('spec/erd.pdf'))
+Attachment.create!(user_id: user1.id, candidate_id: candidate1.id, file: Rack::Test::UploadedFile.new('spec/text.pdf'))
+
+Interview.create!(candidate_id: candidate1.id, user_id: user1.id, status: 1)
+Interview.create!(candidate_id: candidate1.id, user_id: user2.id, status: 1)
+Interview.create!(candidate_id: candidate1.id, user_id: user3.id, status: 1)
+Interview.create!(candidate_id: candidate1.id, user_id: user4.id, status: 0)
+
+Interview.create!(candidate_id: candidate2.id, user_id: user1.id, status: 0)
+Interview.create!(candidate_id: candidate2.id, user_id: user4.id, status: 0)
+
+Interview.create!(candidate_id: candidate3.id, user_id: user2.id, status: 1)
+Interview.create!(candidate_id: candidate3.id, user_id: user1.id, status: 0)
