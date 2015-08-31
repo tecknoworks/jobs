897bc09c7332d8810baab5686cf3271bba23157a
Cristian Mircea Messel <cristian.messel@tecknoworks.com>
2015-08-31 12:09:22 +0300

    remove unsused code

--- db/schema.rb
+++ db/schema.rb
@@ -11,72 +11,75 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20_150_811_110_258) do
-  create_table 'active_admin_comments', force: :cascade do |t|
-    t.string 'namespace'
-    t.text 'body'
-    t.string 'resource_id',   null: false
-    t.string 'resource_type', null: false
-    t.integer 'author_id'
-    t.string 'author_type'
-    t.datetime 'created_at'
-    t.datetime 'updated_at'
+ActiveRecord::Schema.define(version: 20150831085434) do
+
+  create_table "active_admin_comments", force: :cascade do |t|
+    t.string   "namespace"
+    t.text     "body"
+    t.string   "resource_id",   null: false
+    t.string   "resource_type", null: false
+    t.integer  "author_id"
+    t.string   "author_type"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
-  add_index 'active_admin_comments', %w(author_type author_id), name: 'index_active_admin_comments_on_author_type_and_author_id'
-  add_index 'active_admin_comments', ['namespace'], name: 'index_active_admin_comments_on_namespace'
-  add_index 'active_admin_comments', %w(resource_type resource_id), name: 'index_active_admin_comments_on_resource_type_and_resource_id'
+  add_index "active_admin_comments", ["author_type", "author_id"], name: "index_active_admin_comments_on_author_type_and_author_id"
+  add_index "active_admin_comments", ["namespace"], name: "index_active_admin_comments_on_namespace"
+  add_index "active_admin_comments", ["resource_type", "resource_id"], name: "index_active_admin_comments_on_resource_type_and_resource_id"
 
-  create_table 'attachments', force: :cascade do |t|
-    t.integer 'candidate_id',             null: false
-    t.integer 'user_id',                  null: false
-    t.integer 'status',       default: 0
-    t.string 'file',                     null: false
-    t.datetime 'created_at',               null: false
-    t.datetime 'updated_at',               null: false
+  create_table "attachments", force: :cascade do |t|
+    t.integer  "candidate_id",             null: false
+    t.integer  "user_id",                  null: false
+    t.integer  "status",       default: 0
+    t.string   "file",                     null: false
+    t.datetime "created_at",               null: false
+    t.datetime "updated_at",               null: false
   end
 
-  create_table 'candidates', force: :cascade do |t|
-    t.string 'full_name'
-    t.string 'phone_number'
-    t.string 'email'
-    t.datetime 'created_at',   null: false
-    t.datetime 'updated_at',   null: false
-    t.integer 'job_id'
+  create_table "candidates", force: :cascade do |t|
+    t.string   "full_name"
+    t.string   "phone_number"
+    t.string   "email"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+    t.integer  "job_id"
   end
 
-  create_table 'interviews', force: :cascade do |t|
-    t.integer 'candidate_id'
-    t.integer 'user_id'
-    t.integer 'status'
-    t.datetime 'created_at',   null: false
-    t.datetime 'updated_at',   null: false
+  create_table "interviews", force: :cascade do |t|
+    t.integer  "candidate_id"
+    t.integer  "user_id"
+    t.integer  "status"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
   end
 
-  create_table 'jobs', force: :cascade do |t|
-    t.string 'title',                   null: false
-    t.text 'description',             null: false
-    t.datetime 'created_at',              null: false
-    t.datetime 'updated_at',              null: false
-    t.integer 'status',      default: 0
+  create_table "jobs", force: :cascade do |t|
+    t.string   "title",                   null: false
+    t.text     "description",             null: false
+    t.datetime "created_at",              null: false
+    t.datetime "updated_at",              null: false
+    t.integer  "status",      default: 0
   end
 
-  create_table 'users', force: :cascade do |t|
-    t.string 'email',                  default: '',    null: false
-    t.string 'encrypted_password',     default: '',    null: false
-    t.string 'reset_password_token'
-    t.datetime 'reset_password_sent_at'
-    t.datetime 'remember_created_at'
-    t.integer 'sign_in_count',          default: 0,     null: false
-    t.datetime 'current_sign_in_at'
-    t.datetime 'last_sign_in_at'
-    t.string 'current_sign_in_ip'
-    t.string 'last_sign_in_ip'
-    t.boolean 'admin',                  default: false
-    t.datetime 'created_at',                             null: false
-    t.datetime 'updated_at',                             null: false
+  create_table "users", force: :cascade do |t|
+    t.string   "email",                  default: "",    null: false
+    t.string   "encrypted_password",     default: "",    null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          default: 0,     null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+    t.boolean  "admin",                  default: false
+    t.datetime "created_at",                             null: false
+    t.datetime "updated_at",                             null: false
+    t.string   "foo"
   end
 
-  add_index 'users', ['email'], name: 'index_users_on_email', unique: true
-  add_index 'users', ['reset_password_token'], name: 'index_users_on_reset_password_token', unique: true
+  add_index "users", ["email"], name: "index_users_on_email", unique: true
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+
 end
