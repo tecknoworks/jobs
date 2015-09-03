a5a8ae081966371c2139014a9b441259aeabbd5c
Muresan Ionut <muresan.ionut95@gmail.com>
2015-09-03 16:23:38 +0300

    list table with candidates on page with job create candidate from page with job

--- public/js/controllers/UserTKWJobsShowController.js
+++ public/js/controllers/UserTKWJobsShowController.js
@@ -2,6 +2,7 @@ app.controller('UserTKWJobsShowController', function ($scope, $http, $routeParam
   $scope.job = {};
   $scope.attachments = [];
   $scope.attach = {};
+  $scope.candidate = {};
 
   $http.get('api/jobs/'+$routeParams.id).success(function(data){
     $scope.job = data['body'];
@@ -9,6 +10,26 @@ app.controller('UserTKWJobsShowController', function ($scope, $http, $routeParam
     $scope.html_content = markdown.toHTML( md_content );
     $("markout").innerHTML = $scope.html_content
   });
+
+  get_candidates = function(){
+    $http.get('api/jobs/' + $routeParams.id + '/candidates').success(function(data){
+      $scope.candidates = data['body'];
+    });
+  };
+
+  get_candidates()
+
+  $scope.create_candidate = function(){
+    console.log($scope.candidate)
+    $http.post('/api/jobs/' + $scope.job.id + '/candidates', {candidate: $scope.candidate}).
+    success(function(data, status, headers, config) {
+      $scope.rezultat = data;
+      get_candidates();
+  }).
+    error(function(data, status, headers, config) {
+      $scope.rezultat = data;
+    });
+  }
 
   $scope.delete_job = function(id){
     $http.delete('/api/jobs/' + $routeParams.id).success(function(data){
