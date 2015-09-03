c4c9c0086bc07e514b118bfc2df476ea29d15ad5
Muresan Ionut <muresan.ionut95@gmail.com>
2015-09-03 14:15:37 +0300

    add controller for create job add new.html page print input in format html

---
+++ public/js/controllers/UserTKWJobsNewController.js
@@ -0,0 +1,25 @@
+app.controller('UserTKWJobsNewController', function ($scope, $http, $routeParams) {
+  function Editor(input, preview) {
+    this.update = function () {
+      preview.innerHTML = markdown.toHTML(input.value);
+    };
+    input.editor = this;
+    this.update();
+  }
+
+  new Editor($("text-input"), $("markout"));
+
+  $scope.status = 0;
+  $scope.asd = $("text-input").value
+
+  $scope.create = function(){
+    $http.post('/api/jobs', {job: {description: $scope.asd, status: $scope.status}}).
+      success(function(data, status, headers, config) {
+        $scope.rezultat = data;
+        window.location.replace("/user_tkw/jobs/"+data['body']['id']);
+      }).
+      error(function(data, status, headers, config) {
+        $scope.rezultat = data;
+      });
+  }
+});
