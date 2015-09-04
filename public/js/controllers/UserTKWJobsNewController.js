b84a3f305d8a4a7e1e0d527790d711548adbfa3d
Muresan Ionut <muresan.ionut95@gmail.com>
2015-09-04 11:49:03 +0300

    generate title when write job

--- public/js/controllers/UserTKWJobsNewController.js
+++ public/js/controllers/UserTKWJobsNewController.js
@@ -1,24 +1,37 @@
 app.controller('UserTKWJobsNewController', function ($scope, $http, $routeParams) {
-  function Editor(input, preview) {
+  function Editor(title, input, preview) {
     this.update = function () {
+      var title_description = input.value.split('\n')[0]
+      last = title_description.replace(/[^0-9a-z \-\_\.\,]/i, '')
+      while(last != title_description){
+        title_description = last
+        last = title_description.replace(/[^0-9a-z \-\_\.\,]/i, '');
+      };
+      title.value = title_description;
+
       preview.innerHTML = markdown.toHTML(input.value);
     };
     input.editor = this;
     this.update();
   }
 
-  new Editor($("text-input"), $("markout"));
+  new Editor($("title-output"), $("text-input"), $("markout"));
 
   $scope.status = 0;
 
+  $scope.testee = function(){
+    $scope.title += 1
+  }
+
   $scope.create = function(){
     $http.post('/api/jobs', {job: {description: $("text-input").value, status: $scope.status}}).
-      success(function(data, status, headers, config) {
-        $scope.rezultat = data;
-        window.location.replace("/user_tkw/jobs/"+data['body']['id']);
-      }).
-      error(function(data, status, headers, config) {
-        $scope.rezultat = data;
-      });
+    success(function(data, status, headers, config) {
+      $scope.rezultat = data;
+      window.location.replace("/user_tkw/jobs/"+data['body']['id']);
+    }).
+    error(function(data, status, headers, config) {
+      $scope.rezultat = data;
+    });
   }
+
 });
