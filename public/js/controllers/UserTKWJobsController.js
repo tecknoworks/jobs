aece1b884e81f89d42f7f2e3b9543a7ddd619e7f
Muresan Ionut <muresan.ionut95@gmail.com>
2015-09-04 12:56:05 +0300

    print string for status(jobs.html)

--- public/js/controllers/UserTKWJobsController.js
+++ public/js/controllers/UserTKWJobsController.js
@@ -6,6 +6,14 @@ app.controller('UserTKWJobsController', function ($scope, $http, $routeParams) {
     });
   };
 
+  $scope.status = {
+    '0': 'DRAFT',
+    '1': 'PUBLISHED',
+    '2': 'FILLED',
+    '3': 'EXPIRED',
+    '4': 'DASHBOARD'
+  };
+
   get_jobs = function(){
     $http.get('api/jobs').success(function(data){
       $scope.jobs = data['body'];
