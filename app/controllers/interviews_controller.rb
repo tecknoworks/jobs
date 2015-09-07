6d9055b45fa34217752a313e2956bc521056b8da
Muresan Ionut <muresan.ionut95@gmail.com>
2015-09-07 18:16:43 +0300

    index, show, create, update and delete interviews just if user is logged change old tests create new tests

--- app/controllers/interviews_controller.rb
+++ app/controllers/interviews_controller.rb
@@ -1,23 +1,39 @@
 class InterviewsController < ApplicationController
   api :GET, '/api/jobs/:id/candidates/:id/interviews', 'Return list of interviews'
   def index
-    @interviews = Interview.where(candidate_id: params[:candidate_id])
+    if logged(params)
+      @interviews = Interview.where(candidate_id: params[:candidate_id])
+    else
+      render_response('You are not logged', 400_001)
+    end
   end
 
   api :GET, '/api/jobs/:id/candidates/:id/interviews/:id', 'Return an interview'
   def show
-    @interview = Interview.find(params[:id])
+    if logged(params)
+      @interview = Interview.find(params[:id])
+    else
+      render_response('You are not logged', 400_001)
+    end
   end
 
   api :POST, '/api/jobs/:id/candidates/:id/interviews', 'Create an interview'
   def create
-    @interview = Interview.create!(interview_params)
+    if logged(params)
+      @interview = Interview.create!(interview_params)
+    else
+      render_response('You are not logged', 400_001)
+    end
   end
 
   api :DELETE, '/api/jobs/:id/candidates/:id/interviews/:id', 'Delete an interview'
   def destroy
-    @interview = Interview.find(params[:id])
-    @interview.delete
+    if logged(params)
+      @interview = Interview.find(params[:id])
+      @interview.delete
+    else
+      render_response('You are not logged', 400_001)
+    end
   end
 
   private
@@ -26,5 +42,13 @@ class InterviewsController < ApplicationController
     para = params.require(:interview).permit(:user_id, :status)
     para[:candidate_id] = params[:candidate_id]
     para
+  end
+
+  def logged(params)
+    if Key.where(consumer_key: params[:consumer_key], secret_key: params[:secret_key]) == []
+      return false
+    else
+      return true
+    end
   end
 end
--- spec/controllers/interviews_controller_spec.rb
+++ spec/controllers/interviews_controller_spec.rb
@@ -7,9 +7,11 @@ RSpec.describe InterviewsController, type: :controller do
     it 'when candidate not exist' do
       candidate = create :candidate
       user = create :user
+      key = create :key, user_id: user.id
+
       create :interview, candidate_id: candidate.id, user_id: user.id
       create :interview, candidate_id: candidate.id, user_id: user.id
-      get :index, job_id: 1, candidate_id: -1, format: :json
+      get :index, consumer_key: key.consumer_key, secret_key: key.secret_key, job_id: 1, candidate_id: -1, format: :json
       expect(json[:code]).to eq(200)
       expect(json[:body]).to eq([])
     end
@@ -18,24 +20,40 @@ RSpec.describe InterviewsController, type: :controller do
       candidate1 = create :candidate
       candidate2 = create :candidate
       user = create :user
+      key = create :key, user_id: user.id
       create :interview, candidate_id: candidate1.id, user_id: user.id
       create :interview, candidate_id: candidate1.id, user_id: user.id
       create :interview, candidate_id: candidate2.id, user_id: user.id
-      get :index, job_id: 1, candidate_id: candidate1.id, format: :json
+
+      get :index, consumer_key: key.consumer_key, secret_key: key.secret_key, job_id: 1, candidate_id: candidate1.id, format: :json
       expect(json[:code]).to eq(200)
       expect(json[:body].count).to eq(2)
-      get :index, job_id: 1, candidate_id: candidate2.id, format: :json
+
+      get :index, consumer_key: key.consumer_key, secret_key: key.secret_key, job_id: 1, candidate_id: candidate2.id, format: :json
       expect(json[:code]).to eq(200)
       expect(json[:body].count).to eq(1)
+    end
+
+    it 'when candidate exist and user are not logged' do
+      candidate1 = create :candidate
+      candidate2 = create :candidate
+      user = create :user
+      create :interview, candidate_id: candidate1.id, user_id: user.id
+
+      get :index, consumer_key: '', secret_key: '', job_id: 1, candidate_id: candidate1.id, format: :json
+      expect(json[:code]).to eq(400_001)
+      expect(json[:body]).to eq('You are not logged')
     end
   end
 
   describe 'GET show' do
-    it 'works' do
+    it 'when user are logged' do
       candidate = create :candidate
       user = create :user
+      key = create :key, user_id: user.id
       interview = create :interview, candidate_id: candidate.id, user_id: user.id
-      get :show, job_id: 1, candidate_id: candidate.id, id: interview.id, format: :json
+
+      get :show, consumer_key: key.consumer_key, secret_key: key.secret_key, job_id: 1, candidate_id: candidate.id, id: interview.id, format: :json
       expect(json[:code]).to eq(200)
       expect(json[:body][:id]).to eq(1)
       expect(json[:body][:candidate_id]).to eq(1)
@@ -43,29 +61,45 @@ RSpec.describe InterviewsController, type: :controller do
       expect(json[:body][:status]).to eq(1)
     end
 
-    it 'when candidate not exist' do
+    it 'when user are not logged' do
       candidate = create :candidate
       user = create :user
       interview = create :interview, candidate_id: candidate.id, user_id: user.id
-      get :show, job_id: 1, candidate_id: -1, id: interview.id, format: :json
+      get :show, consumer_key: '', secret_key: '', job_id: 1, candidate_id: candidate.id, id: interview.id, format: :json
+      expect(json[:code]).to eq(400_001)
+      expect(json[:body]).to eq('You are not logged')
+    end
+
+    it 'when candidate not exist' do
+      candidate = create :candidate
+      user = create :user
+      key = create :key, user_id: user.id
+      interview = create :interview, candidate_id: candidate.id, user_id: user.id
+
+      get :show, consumer_key: key.consumer_key, secret_key: key.secret_key, job_id: 1, candidate_id: -1, id: interview.id, format: :json
+      expect(json[:code]).to eq(200)
     end
 
     it 'when interview not exist' do
       candidate = create :candidate
       user = create :user
+      key = create :key, user_id: user.id
       interview = create :interview, candidate_id: candidate.id, user_id: user.id
+
       expect do
-        get :show, job_id: 1, candidate_id: -1, id: -1, format: :json
+        get :show, consumer_key: key.consumer_key, secret_key: key.secret_key, job_id: 1, candidate_id: -1, id: -1, format: :json
       end.to raise_error ActiveRecord::RecordNotFound
     end
   end
 
   describe 'POST create' do
-    it 'works' do
+    it 'when user is logged' do
       candidate = create :candidate
       user = create :user
+      key = create :key, user_id: user.id
+
       expect do
-        post :create, job_id: 1, candidate_id: candidate.id, interview: { user_id: user.id, status: 1 }, format: :json
+        post :create, consumer_key: key.consumer_key, secret_key: key.secret_key, job_id: 1, candidate_id: candidate.id, interview: { user_id: user.id, status: 1 }, format: :json
       end.to change { Interview.count }.by 1
       expect(json[:code]).to eq(200)
       expect(json[:body][:user_id]).to eq(user.id)
@@ -73,40 +107,64 @@ RSpec.describe InterviewsController, type: :controller do
       expect(json[:body][:candidate_id]).to eq(candidate.id)
     end
 
-    it 'when status/user_id/candidate_id is invalid' do
+    it 'when user is not logged' do
       candidate = create :candidate
       user = create :user
 
+      post :create, consumer_key: '', secret_key: '', job_id: 1, candidate_id: candidate.id, interview: { user_id: user.id, status: 1 }, format: :json
+      expect(json[:code]).to eq(400_001)
+      expect(json[:body]).to eq('You are not logged')
+    end
+
+    it 'when status/user_id/candidate_id is invalid' do
+      candidate = create :candidate
+      user = create :user
+      key = create :key, user_id: user.id
+
       expect do
-        post :create, job_id: 1, candidate_id: -1, interview: { user_id: user.id, status: 1 }, format: :json
+        post :create, consumer_key: key.consumer_key, secret_key: key.secret_key, job_id: 1, candidate_id: -1, interview: { user_id: user.id, status: 1 }, format: :json
       end.to raise_error ActiveRecord::RecordInvalid
 
       expect do
-        post :create, job_id: 1, candidate_id: candidate.id, interview: { user_id: user.id, status: -1 }, format: :json
+        post :create, consumer_key: key.consumer_key, secret_key: key.secret_key, job_id: 1, candidate_id: candidate.id, interview: { user_id: user.id, status: -1 }, format: :json
       end.to raise_error ActiveRecord::RecordInvalid
 
       expect do
-        post :create, job_id: 1, candidate_id: candidate.id, interview: { user_id: -1, status: 1 }, format: :json
+        post :create, consumer_key: key.consumer_key, secret_key: key.secret_key, job_id: 1, candidate_id: candidate.id, interview: { user_id: -1, status: 1 }, format: :json
       end.to raise_error ActiveRecord::RecordInvalid
     end
   end
 
   describe 'DELETE destroy' do
-    it 'works' do
+    it 'when user is logged' do
+      candidate = create :candidate
+      user = create :user
+      key = create :key, user_id: user.id
+      interview = create :interview, candidate_id: candidate.id, user_id: user.id
+
+      expect do
+        delete :destroy, consumer_key: key.consumer_key, secret_key: key.secret_key, job_id: 1, candidate_id: candidate.id, id: interview.id, format: :json
+      end.to change { Interview.count }.by (-1)
+    end
+
+    it 'when user is not logged' do
       candidate = create :candidate
       user = create :user
       interview = create :interview, candidate_id: candidate.id, user_id: user.id
-      expect do
-        delete :destroy, job_id: 1, candidate_id: candidate.id, id: interview.id, format: :json
-      end.to change { Interview.count }.by (-1)
+
+      delete :destroy, consumer_key: '', secret_key: '', job_id: 1, candidate_id: candidate.id, id: interview.id, format: :json
+      expect(json[:code]).to eq(400_001)
+      expect(json[:body]).to eq('You are not logged')
     end
 
     it 'when interview not exist' do
       candidate = create :candidate
       user = create :user
+      key = create :key, user_id: user.id
       interview = create :interview, candidate_id: candidate.id, user_id: user.id
+
       expect do
-        delete :destroy, job_id: 1, candidate_id: candidate.id, id: -1, format: :json
+        delete :destroy, consumer_key: key.consumer_key, secret_key: key.secret_key, job_id: 1, candidate_id: candidate.id, id: -1, format: :json
       end.to raise_error ActiveRecord::RecordNotFound
     end
   end
