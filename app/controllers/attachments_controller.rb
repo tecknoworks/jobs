ae166c70043784372899f1c470e78036eb112395
Muresan Ionut <muresan.ionut95@gmail.com>
2015-09-09 11:11:23 +0300

    attachment index, show, create, update, delete only if user is logged

--- app/controllers/attachments_controller.rb
+++ app/controllers/attachments_controller.rb
@@ -1,21 +1,33 @@
 class AttachmentsController < ApplicationController
   api :GET, '/attachments', 'Return all attachments'
   def index
-    @attachments = Attachment.where(
-      'created_at >= :days and candidate_id == :candidate_id',
-      days: Time.zone.now - 30.days,
-      candidate_id: params[:candidate_id]
-    )
+    if logged(params)
+      @attachments = Attachment.where(
+        'created_at >= :days and candidate_id == :candidate_id',
+        days: Time.zone.now - 30.days,
+        candidate_id: params[:candidate_id]
+      )
+    else
+      render_response('You are not logged', 400_001)
+    end
   end
 
   api :GET, '/attachments/:id', 'Return attachment by id'
   def show
-    @attachment = Attachment.find(params[:id])
+    if logged(params)
+      @attachment = Attachment.find(params[:id])
+    else
+      render_response('You are not logged', 400_001)
+    end
   end
 
   api :post, '/attachments/:id', 'Create an attachment'
   def create
-    @attachment = Attachment.create!(user_id: params[:user_id], candidate_id: params[:candidate_id], file: params[:attachment])
+    if logged(params)
+      @attachment = Attachment.create!(user_id: params[:user_id], candidate_id: params[:candidate_id], file: params[:attachment])
+    else
+      render_response('You are not logged', 400_001)
+    end
     # unless @attachment.save
     # return render json: @attachment.errors, status: :unprocessable_entity
     # end
@@ -23,19 +35,35 @@ class AttachmentsController < ApplicationController
 
   api :put, '/attachments/:id', 'Update an attachment'
   def update
-    @attachment = Attachment.find(params[:id])
-    @attachment.update_attributes!(user_id: params[:user_id], candidate_id: params[:candidate_id], file: params[:attachment])
+    if logged(params)
+      @attachment = Attachment.find(params[:id])
+      @attachment.update_attributes!(user_id: params[:user_id], candidate_id: params[:candidate_id], file: params[:attachment])
+    else
+      render_response('You are not logged', 400_001)
+    end
   end
 
   api :delete, 'attachments/:id', 'Detele an attachment'
   def destroy
-    @attachment = Attachment.find(params[:id])
-    @attachment.delete
+    if logged(params)
+      @attachment = Attachment.find(params[:id])
+      @attachment.delete
+    else
+      render_response('You are not logged', 400_001)
+    end
   end
 
   private
 
   def attachment_params
     para = params.require(:attachment).permit(:user_id, :candidate_id, :file)
+  end
+
+  def logged(params)
+    if Key.where(consumer_key: params[:consumer_key], secret_key: params[:secret_key]) == []
+      return false
+    else
+      return true
+    end
   end
 end
--- spec/controllers/attachments_controller_spec.rb
+++ spec/controllers/attachments_controller_spec.rb
@@ -13,103 +13,160 @@ RSpec.describe AttachmentsController, type: :controller do
   end
 
   describe 'GET index' do
-    it 'works' do
+    it 'works when user is logged' do
+      key = create :key
       create :attachment, candidate_id: @candidate1.id, user: @user1
       create :attachment, candidate_id: @candidate1.id, user: @user2
       create :attachment, candidate_id: @candidate2.id, user: @user1
 
-      get :index, candidate_id: @candidate1.id, format: :json
+      get :index, consumer_key: key.consumer_key, secret_key: key.secret_key, candidate_id: @candidate1.id, format: :json
       expect(json[:code]).to eq(200)
       expect(json[:body].count).to eq(2)
 
-      get :index, candidate_id: @candidate2.id, format: :json
+      get :index, consumer_key: key.consumer_key, secret_key: key.secret_key, candidate_id: @candidate2.id, format: :json
       expect(json[:code]).to eq(200)
       expect(json[:body].count).to eq(1)
 
-      get :index, candidate_id: -1, format: :json
+      get :index, consumer_key: key.consumer_key, secret_key: key.secret_key, candidate_id: -1, format: :json
       expect(json[:code]).to eq(200)
       expect(json[:body].count).to eq(0)
+    end
+
+    it 'when user is not logged' do
+      create :attachment, candidate_id: @candidate1.id, user: @user1
+      create :attachment, candidate_id: @candidate1.id, user: @user2
+      create :attachment, candidate_id: @candidate2.id, user: @user1
+
+      get :index, consumer_key: '', secret_key: '', candidate_id: @candidate1.id, format: :json
+      expect(json[:code]).to eq(400_001)
+      expect(json[:body]).to eq('You are not logged')
+
+      get :index, consumer_key: '', secret_key: '', candidate_id: @candidate2.id, format: :json
+      expect(json[:code]).to eq(400_001)
+      expect(json[:body]).to eq('You are not logged')
     end
   end
 
   describe 'GET show' do
-    it 'works' do
+    it 'works when user is logged' do
+      key = create :key
       @attachment1 = create :attachment, candidate_id: @candidate1.id, user: @user1
       @attachment2 = create :attachment, candidate_id: @candidate2.id, user: @user2
 
-      get :show, candidate_id: @candidate1.id, id: @attachment1.id, format: :json
+      get :show, consumer_key: key.consumer_key, secret_key: key.secret_key, candidate_id: @candidate1.id, id: @attachment1.id, format: :json
       expect(json[:code]).to eq(200)
       expect(json[:body][:candidate_id]).to eq(@candidate1.id)
     end
 
+    it 'when user is not logged' do
+      @attachment1 = create :attachment, candidate_id: @candidate1.id, user: @user1
+      @attachment2 = create :attachment, candidate_id: @candidate2.id, user: @user2
+
+      get :show, consumer_key: '', secret_key: '', candidate_id: @candidate1.id, id: @attachment1.id, format: :json
+      expect(json[:code]).to eq(400_001)
+      expect(json[:body]).to eq('You are not logged')
+    end
+
     it 'when candidate not exist' do
+      key = create :key
       expect do
-        get :show, candidate_id: @candidate1.id, id: -1, format: :json
+        get :show, consumer_key: key.consumer_key, secret_key: key.secret_key, candidate_id: @candidate1.id, id: -1, format: :json
       end.to raise_error ActiveRecord::RecordNotFound
     end
   end
 
   describe 'POST create' do
-    it 'works' do
+    it 'works when user is logged' do
+      key = create :key
       expect do
-        post :create, candidate_id: @candidate1.id, user_id: @user1.id, attachment: Rack::Test::UploadedFile.new('spec/erd.pdf'), format: :json
+        post :create, consumer_key: key.consumer_key, secret_key: key.secret_key, candidate_id: @candidate1.id, user_id: @user1.id, attachment: Rack::Test::UploadedFile.new('spec/erd.pdf'), format: :json
       end.to change { Attachment.count }.by(1)
     end
 
+    it 'when user is not logged' do
+      post :create, consumer_key: '', secret_key: '', candidate_id: @candidate1.id, user_id: @user1.id, attachment: Rack::Test::UploadedFile.new('spec/erd.pdf'), format: :json
+      expect(json[:code]).to eq(400_001)
+      expect(json[:body]).to eq('You are not logged')
+    end
+
     it 'when user not exist' do
+      key = create :key
       expect do
-        post :create, candidate_id: @candidate1.id, user_id: -1, attachment: Rack::Test::UploadedFile.new('spec/erd.pdf'), format: :json
+        post :create, consumer_key: key.consumer_key, secret_key: key.secret_key, candidate_id: @candidate1.id, user_id: -1, attachment: Rack::Test::UploadedFile.new('spec/erd.pdf'), format: :json
       end.to raise_error ActiveRecord::RecordInvalid
     end
 
     it 'when candidate not exist' do
+      key = create :key
       expect do
-        post :create, candidate_id: -1, user_id: @user1.id, attachment: Rack::Test::UploadedFile.new('spec/erd.pdf'), format: :json
+        post :create, consumer_key: key.consumer_key, secret_key: key.secret_key, candidate_id: -1, user_id: @user1.id, attachment: Rack::Test::UploadedFile.new('spec/erd.pdf'), format: :json
       end.to raise_error ActiveRecord::RecordInvalid
     end
 
     it 'when attachment not exist' do
+      key = create :key
       expect do
-        post :create, candidate_id: @candidate1.id, user_id: @user1.id, attachment: '', format: :json
+        post :create, consumer_key: key.consumer_key, secret_key: key.secret_key, candidate_id: @candidate1.id, user_id: @user1.id, attachment: '', format: :json
       end.to raise_error ActiveRecord::RecordInvalid
     end
   end
 
   describe 'PATCH update' do
-    it 'works' do
+    it 'works when user is logged' do
+      key = create :key
       @attachment = create :attachment, candidate_id: @candidate1.id, user: @user1
-      patch :update, candidate_id: @candidate2.id, user_id: @user2.id, attachment: Rack::Test::UploadedFile.new('spec/erd.pdf'), id: @attachment.id, format: :json
+
+      patch :update, consumer_key: key.consumer_key, secret_key: key.secret_key, candidate_id: @candidate2.id, user_id: @user2.id, attachment: Rack::Test::UploadedFile.new('spec/erd.pdf'), id: @attachment.id, format: :json
       expect(json[:code]).to eq(200)
       expect(json[:body][:id]).to eq(@attachment.id)
       expect(json[:body][:candidate_id]).to eq(@candidate2.id)
       expect(json[:body][:user_id]).to eq(@user2.id)
     end
 
+    it 'when user is not logged' do
+      @attachment = create :attachment, candidate_id: @candidate1.id, user: @user1
+
+      patch :update, consumer_key: '', secret_key: '', candidate_id: @candidate2.id, user_id: @user2.id, attachment: Rack::Test::UploadedFile.new('spec/erd.pdf'), id: @attachment.id, format: :json
+      expect(json[:code]).to eq(400_001)
+      expect(json[:body]).to eq('You are not logged')
+    end
+
     it 'when user not exist' do
+      key = create :key
       @attachment = create :attachment, candidate_id: @candidate1.id, user: @user1
       expect do
-        patch :update, candidate_id: @candidate1.id, user_id: -1, attachment: Rack::Test::UploadedFile.new('spec/erd.pdf'), id: @attachment.id, format: :json
+        patch :update, consumer_key: key.consumer_key, secret_key: key.secret_key, candidate_id: @candidate1.id, user_id: -1, attachment: Rack::Test::UploadedFile.new('spec/erd.pdf'), id: @attachment.id, format: :json
       end.to raise_error ActiveRecord::RecordInvalid
     end
 
     it 'when attachment not exist' do
+      key = create :key
       expect do
-        patch :update, candidate_id: @candidate1.id, user_id: -1, attachment: Rack::Test::UploadedFile.new('spec/erd.pdf'), id: -1, format: :json
+        patch :update, consumer_key: key.consumer_key, secret_key: key.secret_key, candidate_id: @candidate1.id, user_id: -1, attachment: Rack::Test::UploadedFile.new('spec/erd.pdf'), id: -1, format: :json
       end.to raise_error ActiveRecord::RecordNotFound
     end
   end
 
   describe 'DELETE destroy' do
-    it 'works' do
+    it 'works when user is logged' do
+      key = create :key
       @attachment = create :attachment, candidate_id: @candidate1.id, user: @user1
       expect do
-        delete :destroy, candidate_id: @candidate1.id, user_id: @user1.id, attachment: Rack::Test::UploadedFile.new('spec/erd.pdf'), id: @attachment.id, format: :json
+        delete :destroy, consumer_key: key.consumer_key, secret_key: key.secret_key, candidate_id: @candidate1.id, user_id: @user1.id, attachment: Rack::Test::UploadedFile.new('spec/erd.pdf'), id: @attachment.id, format: :json
       end.to change { Attachment.count }.by (-1)
     end
 
+    it 'when user is not logged' do
+      @attachment = create :attachment, candidate_id: @candidate1.id, user: @user1
+      delete :destroy, consumer_key: '', secret_key: '', candidate_id: @candidate1.id, user_id: @user1.id, attachment: Rack::Test::UploadedFile.new('spec/erd.pdf'), id: @attachment.id, format: :json
+      expect(json[:code]).to eq(400_001)
+      expect(json[:body]).to eq('You are not logged')
+    end
+
     it 'when attachment not exist' do
+      key = create :key
       expect do
-        delete :destroy, id: -1, format: :json
+        delete :destroy, consumer_key: key.consumer_key, secret_key: key.secret_key, id: -1, format: :json
       end.to raise_error ActiveRecord::RecordNotFound
     end
   end
