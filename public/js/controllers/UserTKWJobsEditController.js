db5a762578e14a46fd79029adcbb7eb96a6a2cd0
Muresan Ionut <muresan.ionut95@gmail.com>
2015-09-16 18:33:00 +0300

    auto save job when edit

--- public/js/controllers/UserTKWJobsEditController.js
+++ public/js/controllers/UserTKWJobsEditController.js
@@ -14,23 +14,9 @@ app.controller('UserTKWJobsEditController', function ($scope, $http, $routeParam
     this.update();
   }
 
-  $http.get('/api/jobs/' + $routeParams.id + generate_url_key()).
-  success(function(data,status,headers, config){
-    $scope.job = data['body']
-    $scope.select = $scope.keys[$scope.job.status]
-    $scope.status = $scope.job.status
-
-    $("text-input").value = $scope.job.description
-    new Editor($("title-output"), $("text-input"), $("markout"));
-  }).
-  error(function(data){
-    logged(data)
-  })
-
   $scope.save_job = function(){
     $http.put('/api/jobs/' + $scope.job.id + generate_url_key(), {job: {description: $("text-input").value, status: $scope.status_hash[$scope.select]}}).
     success(function(data, status, headers, config) {
-      window.location.replace("/user_tkw/jobs/" + data['body']['id']);
     }).
     error(function(data, status, headers, config) {
       logged(data)
@@ -46,5 +32,24 @@ app.controller('UserTKWJobsEditController', function ($scope, $http, $routeParam
       logged(data);
     });
   };
+
+  auto_save = function (){
+    $scope.save_job();
+    setTimeout(auto_save, 3000);
+  };
+
+  $http.get('/api/jobs/' + $routeParams.id + generate_url_key()).
+  success(function(data,status,headers, config){
+    $scope.job = data['body']
+    $scope.select = $scope.keys[$scope.job.status]
+    $scope.status = $scope.job.status
+
+    $("text-input").value = $scope.job.description
+    new Editor($("title-output"), $("text-input"), $("markout"));
+    auto_save();
+  }).
+  error(function(data){
+    logged(data)
+  })
 
 });
